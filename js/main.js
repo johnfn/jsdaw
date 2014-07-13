92b6b330ad294d6b52a10e27a38b8c08c611118c
johnfn <johnfn@gmail.com>
2014-07-12 22:16:14 -0700

    Add some actions on mouseover.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ var renderPianoRoll = function() {
   for (var j = 0; j < 10; j++) {
     var $row = $("<div>");
 
-    for (var i = 0; i < 10; i++) {
-      var $cell = $("<div>", { "class": "cell" });
-      $row.append($cell);
-    }
+    _.each(_.range(10), function(i) { 
+      var $cell = $("<div>", { "class": "cell" })
+        .appendTo($row)
+        .on('mouseover', function(e) {
+          $cell.addClass("highlighted");
+        })
+        .on('mouseout', function(e) {
+          $cell.removeClass("highlighted");
+        });
+    });
 
     $("#roll").append($row);
   }
