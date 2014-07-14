332fff003a66a46b1a7dc9595d2d5b32730a6076
johnfn <johnfn@gmail.com>
2014-07-13 23:25:21 -0700

    Note names. Also, add some class.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,52 @@
 $(function() {
 
-var renderPianoRoll = function() {
-  for (var j = 0; j < 10; j++) {
-    var $row = $("<div>");
+NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
-    _.each(_.range(10), function(i) { 
-      var $cell = $("<div>", { "class": "cell" })
-        .appendTo($row)
-        .on('mouseover', function(e) {
-          $cell.addClass("highlighted");
-        })
-        .on('mouseout', function(e) {
-          $cell.removeClass("highlighted");
-        });
-    });
+function Cell(el) {
+  this.el = el;
+  this.note = 'A'
+  this.octave = 4;
+  this.time = 0;
+}
 
-    $("#roll").append($row);
-  }
-};
+function PianoRoll() { 
+  var renderPianoRoll = function() {
+    var result = [];
 
-renderPianoRoll();
+    for (var j = 0; j < 10; j++) {
+      var $row = $("<div>");
+      result[j] = [];
+
+      _.each(_.range(10), function(i) { 
+        var $cell = $("<div>", { "class": "cell" })
+          .appendTo($row)
+          .on('mouseover', function(e) {
+            $cell.addClass("highlighted");
+          })
+          .on('mouseout', function(e) {
+            $cell.removeClass("highlighted");
+          });
+
+        if (i == 0) {
+          $cell.html(NOTES[j]);
+        } else {
+          $cell.html("&nbsp;"); // why is this necessary?
+        }
+
+        var data = new Cell($cell);
+      });
+
+      $("#roll").append($row);
+    }
+  };
+
+  renderPianoRoll();
+}
+
+function main() { 
+  var pr = new PianoRoll();
+}
+
+main();
 
 });
