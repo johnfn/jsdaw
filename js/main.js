b90f549dbbea76df7a9bd185a157ffcfcbc1e8d3
johnfn <johnfn@gmail.com>
2014-07-14 23:11:08 -0700

    Start doing dragging stuff.

--- js/main.js
+++ js/main.js
@@ -13,18 +13,37 @@ function PianoRoll() {
   var renderPianoRoll = function() {
     var result = [];
 
-    for (var j = 0; j < 10; j++) {
+    _.each(_.range(10), function(j) {
       var $row = $("<div>");
       result[j] = [];
+
+      var selectionStart = [];
+      var isSelecting = false;
 
       _.each(_.range(10), function(i) { 
         var $cell = $("<div>", { "class": "cell" })
           .appendTo($row)
           .on('mouseover', function(e) {
             $cell.addClass("highlighted");
+
+            if (isSelecting) {
+              var start = Math.min(selectionStart[0], i);
+              var end = Math.max(selectionStart[0], i);
+
+              for (var k = start; k <= end; k++) {
+                result[j][k].addClass('highlighted');
+              }
+            }
           })
           .on('mouseout', function(e) {
             $cell.removeClass("highlighted");
+          })
+          .on('mousedown', function(e) {
+            isSelecting = true;
+            selectionStart = [i, j];
+          })
+          .on('mouseup', function(e) {
+            isSelecting = false;
           });
 
         if (i == 0) {
@@ -34,10 +53,13 @@ function PianoRoll() {
         }
 
         var data = new Cell($cell);
+
+        result[j].push($cell);
       });
 
       $("#roll").append($row);
-    }
+    });
+
   };
 
   renderPianoRoll();
